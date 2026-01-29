4350edc1a454c23f8f99a18d0d039483f31642af
prof-salu <salustiano.oliveira@prof.infnet.edu.br>
2026-01-29 16:08:42 -0300

    Finalizado o conteudo da semana 01.
    - Criação do mundo;
    - Configuração ods sprites;
    - Movimentação do personagem;
    - Inventário;

---
+++ Assets/Scripts/MovimentacaoExploracao.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class MovimentacaoExploracao : MonoBehaviour
+{
+    public float velocidade = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movimento;
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = rb.GetComponent<SpriteRenderer>();
+        anim = rb.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //1. Captura o input do jogador (SEMPRE NO UPDATE)
+        movimento.x = Input.GetAxisRaw("Horizontal");
+        movimento.y = Input.GetAxisRaw("Vertical");
+
+        //2. Controle da animação
+        if(movimento != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", movimento.x);
+            anim.SetFloat("Vertical", movimento.y);
+            anim.SetBool("Andando", true);
+
+            if (movimento.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (movimento.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+        else
+        {
+            anim.SetBool("Andando", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Aplica a física ao play
+        //.normalized evita que andar na diagonal seja mais rapido
+        rb.MovePosition(rb.position + movimento.normalized * velocidade * Time.fixedDeltaTime);
+    }
+}
